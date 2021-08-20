1ec5fea95c4993e3c8977b9749ddbc710f643e5d
MrZoraman <MrZoraman@gmail.com>
2021-08-20 12:48:46 -0700

    Tear pretty much everything out

--- cmake/blyss.cmake
+++ cmake/blyss.cmake
@@ -1,19 +1,46 @@
 # --------------------------------------------------------- #
 # Variables                                                 #
 # --------------------------------------------------------- #
-set(BLYSS_ARTIFACT_NAME blyss)
-set(BLYSS_MAIN ${CMAKE_CURRENT_SOURCE_DIR}/src/main.cpp)
+set(BLYSS_ARTIFACT_NAME BLYSS)
+set(BLYSS_SRC_DIR ${CMAKE_CURRENT_SOURCE_DIR}/src)
+set(BLYSS_INCLUDE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/include)
+set(BLYSS_SOURCES "${BLYSS_SRC_DIR}/main.cpp")
+set(BLYSS_HEADERS "")
 
 # --------------------------------------------------------- #
-# Create Executable                                         #
+# Macros                                                    #
 # --------------------------------------------------------- #
-add_executable(${BLYSS_ARTIFACT_NAME} ${BLYSS_MAIN})
+macro(BLYSS_add_source SOURCE_NAME)
+    list(APPEND BLYSS_SOURCES ${BLYSS_SRC_DIR}/${SOURCE_NAME}.cpp)
+    list(APPEND BLYSS_HEADERS ${BLYSS_INCLUDE_DIR}/${SOURCE_NAME}.hpp)
+endmacro()
+
+macro(BLYSS_add_header HEADER_NAME)
+    list(APPEND BLYSS_HEADERS ${BLYSS_INCLUDE_DIR}/${HEADER_NAME}.hpp)
+endmacro()
+
+# --------------------------------------------------------- #
+# Sources                                                   #
+# --------------------------------------------------------- #
+
+
+# --------------------------------------------------------- #
+# Visual Studio Folder Hierarchy                            #
+# --------------------------------------------------------- #
+source_group(TREE ${CMAKE_CURRENT_SOURCE_DIR}
+         PREFIX "sources"
+         FILES ${BLYSS_SOURCES} ${BLYSS_HEADERS})
+
+# --------------------------------------------------------- #
+# Create Library                                            #
+# --------------------------------------------------------- #
+add_executable(${BLYSS_ARTIFACT_NAME} ${BLYSS_SOURCES} ${BLYSS_HEADERS})
 
 # --------------------------------------------------------- #
 # C17 standard                                              #
 # --------------------------------------------------------- #
 set_property(TARGET ${BLYSS_ARTIFACT_NAME} PROPERTY CXX_STANDARD 17)
-set_property(TARGET ${BLYSS_ARTIFACT_NAME} PROPERTY CXX_STANDARD_REQUIRED ON)
+set_property(TARGET ${BLYSS_ARTIFACT_NAME} PROPERTY Cxx_STANDARD_REQUIRED ON)
 
 # --------------------------------------------------------- #
 # Set compile options                                       #
@@ -25,8 +52,24 @@ else()
 endif()
 
 # --------------------------------------------------------- #
+# Includes                                                  #
+# --------------------------------------------------------- #
+target_include_directories(${BLYSS_ARTIFACT_NAME} PUBLIC
+    ${BLYSS_INCLUDE_DIR}
+    ${Boost_INCLUDE_DIRS}
+    lib/gsl/include
+    lib/assimp/contrib/irrXML
+)
+
+# --------------------------------------------------------- #
 # Link Libraries                                            #
 # --------------------------------------------------------- #
 target_link_libraries(${BLYSS_ARTIFACT_NAME}
-    ${LIBBLYSS_ARTIFACT_NAME}
+    ${Boost_LIBRARIES}
+    glfw
+    Glad
+    Imgui
+    assimp::assimp
+    glm
+    IrrXML
 )
